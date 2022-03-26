5d910811feb99a60caa7ae6caa6e127907dd0fbe
vanekarenina <90297907+vanekarenina@users.noreply.github.com>
2022-03-26 17:18:58 +0700

    Update Tugas Modul 4.py

--- Tugas Modul 4.py
+++ Tugas Modul 4.py
@@ -1,1 +1,20 @@
+class   mtd_tiket: 
 
+    def __init__(self, tiket): 
+
+        self.tiket = tiket 
+
+
+    def identitas(kelompok): 
+
+        print("\nKelompok\t:",(kelompok)) 
+
+        print("Shift\t\t: 2") 
+
+        print("Nama Anggota 1\t: Vane Karenina Thetan Hartono (21120121120005)") 
+
+        print("Nama Anggota 2\t: Muhammad Farhan Al Hussein (21120121140086)") 
+
+        print("Nama Anggota 3\t: Genaru Badra Kurniawan (21120121130059)") 
+
+        print("Nama Anggota 4\t: Khrisna Fauzan Maulana (21120121140145)") 
